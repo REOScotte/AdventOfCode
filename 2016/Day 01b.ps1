1976c7ba8c84900bc779a4dddb786ca798503022
REOScotte <11577865+REOScotte@users.noreply.github.com>
2017-08-12 15:35:37 -0500

    Cleaned up Days 1,2,3 to use remote file for input. Also cleaned up the answer outputs to finish all calculations.

--- 2016/Day 01b.ps1
+++ 2016/Day 01b.ps1
@@ -1,4 +1,4 @@
-﻿$inputs = "R1, R3, L2, L5, L2, L1, R3, L4, R2, L2, L4, R2, L1, R1, L2, R3, L1, L4, R2, L5, R3, R4, L1, R2, L1, R3, L4, R5, L4, L5, R5, L3, R2, L3, L3, R1, R3, L4, R2, R5, L4, R1, L1, L1, R5, L2, R1, L2, R188, L5, L3, R5, R1, L2, L4, R3, R5, L3, R3, R45, L4, R4, R72, R2, R3, L1, R1, L1, L1, R192, L1, L1, L1, L4, R1, L2, L5, L3, R5, L3, R3, L4, L3, R1, R4, L2, R2, R3, L5, R3, L1, R1, R4, L2, L3, R1, R3, L4, L3, L4, L2, L2, R1, R3, L5, L1, R4, R2, L4, L1, R3, R3, R1, L5, L2, R4, R4, R2, R1, R5, R5, L4, L1, R5, R3, R4, R5, R3, L1, L2, L4, R1, R4, R5, L2, L3, R4, L4, R2, L2, L4, L2, R5, R1, R4, R3, R5, L4, L4, L5, L5, R3, R4, L1, L3, R2, L2, R1, L3, L5, R5, R5, R3, L4, L2, R4, R5, R1, R4, L3"
+﻿$inputs = Get-Content '.\Day 01.txt'
 #$inputs = "R8, R4, R4, R8"
 $inputs = $inputs.Replace(" ", "")
 $inputs = $inputs.Split(",")
@@ -32,4 +32,7 @@ foreach ($input in $inputs) {
     #"$input Turn $facingDirection, walk $l end at X$coordX Y$coordY. Distance = " + [int]([math]::abs($coordX) + [math]::abs($coordY))
 }
 
-"First repeat is " + $repeats[0]
+$repeatX = $repeats[0].Split(' ').Replace('X', '')[0]
+$repeatY = $repeats[0].Split(' ').Replace('Y', '')[1]
+
+"First repeat is " + $repeats[0] + ". Distance = " + [int]([math]::abs($repeatX) + [math]::abs($repeatY))
