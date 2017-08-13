2cb9be0dc6a6212f8201b6f4b252e88334397462
REOScotte <11577865+REOScotte@users.noreply.github.com>
2017-08-13 16:28:29 -0500

    Day 7a complete

---
+++ 2016/Day 07a.ps1
@@ -0,0 +1,27 @@
+﻿$inputs = Get-Content '.\Day 07.txt'
+#$inputs = @"
+#abba[mnop]qrst
+#abcd[bddb]xyyx
+#aaaa[qwer]tyui
+#ioxxoj[asdfgh]zxcvbn
+#"@
+
+$inputs = $inputs.Split("`n")
+$answer = 0
+
+foreach ($input in $inputs) {
+    $valid = 1
+    $DQ = 0
+    $abba = 0
+    for ($i = 0; $i -lt ($input.Length -4); $i++) {
+        if ($input[$i] -eq "[") {$valid = 0}
+        if ($input[$i] -eq "]") {$valid = 1}
+        if ($input[$i] -eq $input[$i + 3] -and $input[$i + 1] -eq $input[$i + 2]) {
+            if (-not $valid) {$DQ = 1}
+            $abba = 1
+        }
+    }
+    if ($abba -and -not $DQ) {$answer++}
+}
+
+"Answer: $answer"
