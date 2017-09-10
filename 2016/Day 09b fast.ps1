5c3538e6031de777d6472f8d2233295fd45f64f1
REOScotte <11577865+REOScotte@users.noreply.github.com>
2017-09-09 22:11:50 -0500

    Completed Day 9

--- 2016/Day 09b fast.ps1
+++ 2016/Day 09b fast.ps1
@@ -1,12 +1,15 @@
-cls
 $inputs = Get-Content '.\Day 09.txt'
-$inputs = @"
-(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7).EVEN
-"@
+#$inputs = @"
+#(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7).EVEN
+#"@
 
-while ($inputs -like "*(*" -and $inputs -like "*)*") {
+function Get-Length {
+    param (
+        [string]$inputs
+    )
+
     $length = $inputs.Length
-    $output = ""
+    $output = 0
     $i = 0
 
     while ($i -lt $length) {
@@ -20,18 +23,18 @@ while ($inputs -like "*(*" -and $inputs -like "*)*") {
             }
             $l = $r.Split('x')[0].Replace('(', '')
             $t = $r.Split('x')[1].Replace(')', '')
-            for ($j = 0; $j -lt $t; $j++) {
-                $output += $inputs.Substring($i, $l)
-            }
+#            for ($j = 0; $j -lt $t; $j++) {
+                $output += (Get-Length -inputs $inputs.Substring($i, $l)) * $t
+#            }
             $i += $l
         }
         else {
-            $output += $c
+            $output += 1
             $i++
         }
     }
-    $inputs = $output
-    $output
-    "`n"
+
+    return $output
 }
 
+Get-Length $inputs
